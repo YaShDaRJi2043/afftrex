c639624700a365b55e291ff57adac41930476daf
YaShDaRJi2043 <yashdarji1955@gmail.com>
2025-08-12 04:13:16 +0530

    Add cookie-parser middleware for enhanced cookie management

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require("express");
 const cors = require("cors");
 const helmet = require("helmet");
 const multer = require("multer");
+const cookieParser = require("cookie-parser"); // Added cookie-parser
 
 const router = require("@routes/routes");
 const { serverInfo } = require("@config/config");
@@ -14,6 +15,7 @@ app.set("trust proxy", true);
 app.use(express.json());
 app.use(cors());
 app.use(helmet());
+app.use(cookieParser()); // Added cookie-parser middleware
 
 app.use(serverInfo.host_url_prefix, router);
 app.use("/public", redirectRoutes);
