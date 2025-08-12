bf7c0e4d1e158066a4af7d7c453e24888a975244
YaShDaRJi2043 <yashdarji1955@gmail.com>
2025-08-12 15:07:38 +0530

    Change Pixel route from POST to GET for image/iframe tracking

--- routes/pixelTracking.routes.js
+++ routes/pixelTracking.routes.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const pixelTrackingController = require("@controllers/pixelTracking/pixelTracking.controller");
 
 // Pixel GET route (for image/iframe tracking)
-router.post("/:slug", pixelTrackingController.handlePixelGet);
+router.get("/:slug", pixelTrackingController.handlePixelGet);
 
 // Pixel POST route (for server_postback/web_sdk tracking)
 // router.post("/:slug", pixelTrackingController.handlePixelPost);
