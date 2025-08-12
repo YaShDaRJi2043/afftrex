e2eb5d93c90755cb8bf32316d2f793ee3bc4e6fe
YaShDaRJi2043 <yashdarji1955@gmail.com>
2025-08-12 11:47:57 +0530

    Remove cookie handler middleware from campaign redirect route

--- routes/redirect.route.js
+++ routes/redirect.route.js
@@ -2,6 +2,6 @@ const router = require("express").Router();
 const redirectController = require("@controllers/redirect/redirect.controller");
 const cookieHandler = require("@middleware/cookieHandler");
 
-router.get("/c/:campaignId", cookieHandler, redirectController.handleRedirect);
+router.get("/c/:campaignId", redirectController.handleRedirect);
 
 module.exports = router;
