bce2c90c5f20750b76af45723430b0d60065a2bf
YaShDaRJi2043 <yashdarji1955@gmail.com>
2025-08-18 22:31:33 +0530

    Refactor storeClickId.js and storeClickId.ejs to ensure click_id is appended to the redirect URL for improved tracking

--- public/js/storeClickId.js
+++ public/js/storeClickId.js
@@ -3,7 +3,7 @@ document.addEventListener("DOMContentLoaded", () => {
 
   // Extract data from the body tag's data-* attributes
   const clickId = body.getAttribute("data-click-id");
-  const redirectUrl = body.getAttribute("data-redirect-url");
+  let redirectUrl = body.getAttribute("data-redirect-url");
 
   // Store the click ID in a cookie
   document.cookie = `click_id=${clickId}; path=/; max-age=${
@@ -13,8 +13,11 @@ document.addEventListener("DOMContentLoaded", () => {
   // Store the click ID in localStorage
   localStorage.setItem("click_id", clickId);
 
+  // Append click_id to the redirect URL
+  redirectUrl += `?click_id=${clickId}`;
+
   // Redirect to the target URL
   setTimeout(() => {
-    window.location.href = redirectUrl;
+    window.location.href = redirectUrl; // Redirect with click_id in the query string
   }, 2000); // Wait for 2 seconds before redirecting
 });
