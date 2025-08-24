b91f8709100739de5951b35602ebabfa2b0e549f
YaShDaRJi2043 <yashdarji1955@gmail.com>
2025-08-24 23:57:18 +0530

    Refactor campaign and pixel tracking retrieval to improve query options handling and maintainability

--- services/report.service.js
+++ services/report.service.js
@@ -2,9 +2,9 @@ const { CampaignTracking, PixelTracking, Publisher } = require("@models");
 
 exports.getCampaignTrackingByCampaignId = async (req) => {
   const { campaignId } = req.params;
+  const { page = 1, pageSize = 10 } = req.query; // Default to page 1 and pageSize 10
 
-  const trackings = await CampaignTracking.findAll({
-    where: { campaignId },
+  const options = {
     include: [
       {
         model: Publisher,
@@ -12,17 +12,33 @@ exports.getCampaignTrackingByCampaignId = async (req) => {
         attributes: ["id", "name"],
       },
     ],
-  });
+    limit: parseInt(pageSize),
+    offset: (parseInt(page) - 1) * parseInt(pageSize),
+  };
+
+  if (campaignId) {
+    options.where = { campaignId };
+  }
+
+  const trackings = await CampaignTracking.findAll(options);
 
   return trackings;
 };
 
 exports.getPixelTrackingByTrackingId = async (req) => {
   const { campaignId } = req.params;
+  const { page = 1, pageSize = 10 } = req.query; // Default to page 1 and pageSize 10
 
-  const pixelTrackings = await PixelTracking.findAll({
-    where: { campaignId },
-  });
+  const options = {
+    limit: parseInt(pageSize),
+    offset: (parseInt(page) - 1) * parseInt(pageSize),
+  };
+
+  if (campaignId) {
+    options.where = { campaignId };
+  }
+
+  const pixelTrackings = await PixelTracking.findAll(options);
 
   return pixelTrackings;
 };
